ad5e0e48cd07155fed411551bc609875a8904d0d
Viraj Tilekar <67374433+VirajTilekar@users.noreply.github.com>
2021-10-01 23:40:32 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
+//CPP code for practice.
+
 #include<iostream>
 using namespace std;
-int main(){
-    int a=2,b=100;
-    for(int i=a;i<b;i++){
-        if(a%i==1)
+
+int main() {
+    int a = 5,b = 150;
+    for(int i = a; i < b; i++) {
+        if(a % i == 1)
             cout<<i;
     }
-
 }
