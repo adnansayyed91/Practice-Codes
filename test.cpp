0ea573864f5a392e503738eeee505bfa5d1d6a0e
atharvapawar181 <70618288+atharvapawar181@users.noreply.github.com>
2021-10-03 11:16:48 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,4 +9,26 @@ int main() {
         if(a % i == 1)
             cout<<i;
     }
+int main() {
+    int year;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                cout << year << " is a leap year.";
+            else
+                cout << year << " is not a leap year.";
+        }
+        else
+            cout << year << " is a leap year.";
+    }
+    else
+        cout << year << " is not a leap year.";
+
+    return 0;
+}
+
 }
